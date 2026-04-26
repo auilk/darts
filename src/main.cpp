85b6abaefed633bc0e1f170ea761aa222d021f37
auilk <abdelghafour.lbaz01@gmail.com>
2026-04-26 21:27:00 +0100

    feat: add GLFW/GLEW OpenGL test window

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        return -1;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
